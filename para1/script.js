27e91e8596a079c0bd941e234bc6b1445a0422a5
MrSlime <Slime.kostia@gmail.com>
2026-09-07 11:11:59 +0300

    Lesson_1

---
+++ para1/script.js
@@ -0,0 +1,45 @@
+// var a = 19;
+// console.log(a);
+
+// let b = "str";
+// const c = 25;
+
+// console.log(a+b);
+// console.log(a-b); 
+// console.log(a*b); 
+// console.log(a**b); 
+// console.log(a/b); 
+// console.log(a%b); 
+// console.log(a*b); 
+// console.log(a*b); 
+  
+// a+=1;
+// a-=1;
+// a*=1;
+// a/=1;
+// a%=1;
+
+// a++;
+// a--;
+
+let a = "10";
+let b = 20;
+
+console.log(a+b);
+console.log(b+a);
+console.log((a-0)+b);
+
+let name = "John";
+let age = 30;   
+
+console.log("My name is " + name + "! I am " + age + " years old.");
+console.log(`My name is ${name}! I am ${age} years old.`);
+
+console.info("This is an info message.");
+console.warn("This is a warning message.");
+console.error("This is an error message.");
+
+let text = prompt("Boo 1");
+let text2 = prompt("Boo 2");
+
+alert(`Boo 1 + Boo 2 = ${parseInt(text) + parseInt(text2)}`);
